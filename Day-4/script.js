cbad6c3cafb69ebc9088057e1fcd85a76a0ecc95
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-09 15:53:06 +0530

    writing function in camelCase and change in return statement

--- Day-4/script.js
+++ Day-4/script.js
@@ -1,40 +1,33 @@
 // A function that adds two numbers
-function add(a, b) {
+function addNumbers(a, b) {
     return a + b;
 }
-let sum = add(2,2);
+let sum = addNumbers(2,2);
 console.log("sum" , sum);
 
 
 // A function that checks if a number is even or odd
-function OddEven(a, b) {
+function checkEvenOdd(a, b) {
    let num = a + b;
-   return num % 2 == 0 ? `Even` : `Odd`
+   return num % 2 === 0 ? "Even" : "Odd"
 }
 
-let result = OddEven(12,2);
+let result = checkEvenOdd(12,2);
 console.log("result" , result);
 
 
 // A function that finds the largest of three numbers
-function largestNum(a, b, c){
-    if(a>b && a>c){
-        return `num1 is greater then num2 and num3`
-    }else if(b>a && b>c){
-        return `num2 is greater then num1 and num3`
-    }else{
-        return `num3 is greater then num1 and num2`
-    }
+function findLargest(a, b, c){
+    return Math.max(a,b,c)
 }
 
-console.log(largestNum(111,21,3));
+console.log(findLargest(111,1112,3));
 
 // A function that converts Celsius to Fahrenheit
-function converter(c){
-    let result = `Celsius ${c} in Fahrenheit is ${(c * 9/5) + 32}`;
-    return result;
+function celsiusToFahrenheit(c){
+    return (c * 9 /5) + 32;
 }
-console.log(converter(20));
+console.log(celsiusToFahrenheit(20));
 
 
 
