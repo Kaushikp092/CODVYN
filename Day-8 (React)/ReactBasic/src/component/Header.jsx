272913da45fc9081d7eeeb7a3d369a4b5a785e28
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-12 20:12:46 +0530

    Learning React

---
+++ Day-8 (React)/ReactBasic/src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+   return (
+      <>
+         <div>Learning React Basic</div>
+         <p>
+            React is library not a framework. React helps in building fast,
+            interactive and reusable user interfaces, especially single-page
+            Applications
+         </p>
+      </>
+   );
+};
+
+export default Header;
