1cbac2887f3136df6f57d1059fdbf13b24701f85
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-19 19:18:23 +0530

    Day - 13 react API & Effects part2

--- Day-8 (React)/ReactBasic/src/components/UserList.jsx
+++ Day-8 (React)/ReactBasic/src/components/UserList.jsx
@@ -1,30 +1,48 @@
 import React, { useState, useEffect } from "react";
 
 const UserList = () => {
-   //       Task 1
    const [userData, setUserData] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
 
    useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/users")
-         .then((res) => res.json()) //fetch the raw respone data and convert into json format
-         .then((data) => setUserData(data)) //passing data in userData using setuserData
-         .catch((err) => console.log(err));
-   }, []); //empty array means it only onces where dom mounted
+      //using async await inside useEffect and also using Try catch
+      const fetchData = async () => {
+         try {
+            const res = await fetch(
+               "https://jsonplaceholder.typicode.com/users",
+            );
+            if (!res.ok) throw new Error("Network response was not ok"); //if res is not ok then it's throw error if anything wrong in api call after user Query then this msg will pop up in ui
+            const data = await res.json();
+            setUserData(data);
+         } catch (err) {
+            setError(err.message);
+         } finally {
+            setLoading(false);
+         }
+      };
+      fetchData();
+   }, []);
 
    return (
       <>
-         <h1>Fetching and Printing data</h1>
-         {userData ? (
+         <h2>Fetching and Printing data</h2>
+         {loading && <p>Loading...</p>}
+
+         {/*or other wise this will shown up */}
+         {error && <p style={{ color: "red" }}>Error: {error}</p>}
+
+         {/* if everthing works well then only data will be shown in ui */}
+         {!loading && !error && (
             <div>
                {userData.map((data) => (
-                  <ul key={data.id}>
-                     {data.name}
-                     {data.email}
+                  <ul key={data.id} style={{ listStyle: "none" }}>
+                     <li>Username: {data.name}</li>
+                     <li>Phone Number: {data.phone}</li>
+                     <li>Company Name: {data.company.name}</li>
                   </ul>
                ))}
             </div>
-         ) : (
-            <p>Loading...</p>
          )}
       </>
    );
