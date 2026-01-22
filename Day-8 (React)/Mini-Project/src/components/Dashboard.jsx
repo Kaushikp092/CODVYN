1c4b5e88e37750852744a84ee04faa6acb68745b
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-22 20:05:31 +0530

    removed material UI component

--- Day-8 (React)/Mini-Project/src/components/Dashboard.jsx
+++ Day-8 (React)/Mini-Project/src/components/Dashboard.jsx
@@ -1,16 +1,10 @@
 import { useState, useEffect } from "react";
 import "./Dashboard.css";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import CardActionArea from "@mui/material/CardActionArea";
-import Button from "@mui/material/Button";
 import LimitSelector from "./LimitSelector";
 import PageSelector from "./PageSelector";
 
 const Dashboard = () => {
-   const [userData, setUserData] = useState([]);
+   const [photos, setPhotos] = useState([]);
    const [loading, setLoading] = useState(true);
    const [error, setError] = useState(null);
    const [limit, setLimit] = useState(10);
@@ -24,7 +18,7 @@ const Dashboard = () => {
             );
             if (!res.ok) throw new Error("Network connection failed");
             const data = await res.json();
-            setUserData(data.photos || []);
+            setPhotos(data.photos || []);
          } catch (err) {
             setError(err.message);
          } finally {
@@ -32,56 +26,30 @@ const Dashboard = () => {
          }
       };
       fetchData();
-   }, [limit,page]);
-
-   const handleChange = () => {
-      window.location.reload();
-   };
+   }, [limit, page]);
 
    return (
       <>
          <div className="header">
             <h2>Fetching Cards detail info</h2>
-            <Button variant="contained" size="medium" onClick={handleChange}>
-               Refresh
-            </Button>
             <LimitSelector limit={limit} onLimitChange={setLimit} />
             <PageSelector page={page} onPageChange={setPage} />
          </div>
          <div className="container">
             {loading && <p>Loading...</p>}
-            {error && <p style={{ color: "red" }}>Error: {error}</p>}
+            {error && <p>Error: {error}</p>}
             {!loading && !error && (
-               <>
-                  <div className='card-grid'>
-                     {userData.map((data) => (
-                        <Card className='card' key={data.id} sx={{ maxWidth: 345 }}>
-                           <CardActionArea>
-                              <CardMedia
-                                 component="img"
-                                 height="140"
-                                 image={data.url}
-                              />
-                              <CardContent>
-                                 <Typography
-                                    gutterBottom
-                                    variant="h5"
-                                    component="div"
-                                 >
-                                    {data.title}
-                                 </Typography>
-                                 <Typography
-                                    variant="body2"
-                                    sx={{ color: "text.secondary" }}
-                                 >
-                                    {data.description}
-                                 </Typography>
-                              </CardContent>
-                           </CardActionArea>
-                        </Card>
-                     ))}
-                  </div>
-               </>
+               <div className="card-grid">
+                  {photos.map((photo) => (
+                     <div className="card" key={photo.id}>
+                        <img src={photo.url} />
+                        <div className="card-content">
+                           <h2>{photo.title}</h2>
+                           <p>{photo.description}</p>
+                        </div>
+                     </div>
+                  ))}
+               </div>
             )}
          </div>
       </>
