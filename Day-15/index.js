722f62fbf5de39678d32475fe2fa08ba15e3d6f9
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-22 21:05:27 +0530

    returning json

--- Day-15/index.js
+++ Day-15/index.js
@@ -2,18 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) =>{
+app.get('/', (req,res) =>{
     res.send('Home page');
 })
 
-app.get('/api/hello', (req, res) => {
-  res.send({name: 'Kaushik',
-    message: 'started learning RESTfull APIs'
-  });
+app.get('/api/hello', (req,res)=>{
+    res.json({message:'Hello from API'});
 })
 
 
 app.listen(port, () => {
-    console.log(`App started on port http://localhost:${port}`);
+    // console.log(`App started on port http://localhost:${port}`);
     
 });
