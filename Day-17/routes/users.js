2fef6cac43abb0d74991feec9abb616d4480c72d
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-28 21:43:27 +0530

    setting all routes

---
+++ Day-17/routes/users.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../modules/users');
+
+// Fetch all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Create a single user
+router.post('/', async (req, res) => {
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        });
+        console.log(user);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Get user by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const userid = await User.findById(req.params.id);
+        res.status(200).json(userid);
+    } catch (err) {
+        res.status(404).json({
+            message: err.message
+        });
+    }
+});
+
+// PUT request
+router.put('/:id', async (req, res) => {
+    try {
+        const userId = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!userId) {
+            return res.status(404).json({
+                message: 'user not found'
+            })
+        }
+        res.status(200).json(userId)
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// DELETE request
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).json({
+                message: 'user not found'
+            });
+        }
+        res.status(200).json({
+            message:'user deleted successfully'
+        })
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+});
+
+module.exports = router;
