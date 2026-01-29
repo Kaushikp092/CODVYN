f444c12fad7b2c8ff405ad6969d5d61b100209a3
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-29 18:45:03 +0530

    Refactor user creation endpoint and remove unused user routes

--- Day-16/routes/users.js
+++ Day-16/routes/users.js
@@ -51,67 +51,12 @@ router.post("/", (req, res) => {
         name: req.body.name,
         email: req.body.email,
     };
-    
+
     users.push(newUser);
     res.status(201).json({
         message: "User created successfully :)",
         user,
     });
 });
-
-// PUT /api/users/:id updates a user by id
-router.put("/:id", (req, res) => {
-    const user = users.find((u) => u.id === parseInt(req.params.id));
-    if (!user) {
-        return res.status(404).json({
-            message: "user not found",
-        });
-    }
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-
-    res.status(200).json({
-        message: "user updated successfully",
-        user,
-    });
-});
-
-//PATCH /api/users/:id partially updates a user by id
-router.patch("/:id", (req, res) => {
-    const user = users.find((u) => u.id === parseInt(req.params.id));
-
-    if (!user) {
-        return res.status(404).json({
-            message: "user not found",
-        });
-    }
-
-    if (req.body.name) user.name = req.body.name;
-    if (req.body.email) user.email = req.body.email;
-
-    res.status(200).json({
-        message: "user patched successfully",
-        user,
-    });
-});
-
-//DELETE /api/users/:id deletes a user by id
-router.delete('/:id',(req,res)=>{
-    const user = users.findIndex(u => u.id === parseInt(req.params.id));
-
-    if(user === -1){
-        return res.status(404).json({
-            message : 'user not found',
-        })
-    }
-
-    const deletedUser = users.splice(user, 1);
-
-    res.status(200).json({
-        message: 'user deleted successfully',
-        user: deletedUser[0]
-    })
-
-})
 
 module.exports = router;
