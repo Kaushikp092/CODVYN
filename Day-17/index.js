42728a3c9396f3e3c495acc2704f30c8c93102a7
kaushikp092 <kaushikpatil092@gmail.com>
2026-01-31 22:51:43 +0530

    add /api/auth route

--- Day-17/index.js
+++ Day-17/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth')
+const userRouter = require('./routes/users');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
@@ -7,12 +9,12 @@ const port = process.env.PORT;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.USER)
     .then(() => console.log('MongoDB connected locally'))
     .catch(err => console.log(err))
 
-const userRouter = require('./routes/users');
-
 app.use('/api/users', userRouter);
+
+app.use('/api/auth', authRoutes)
 
 app.listen(port, () => console.log(`Example app listening http://localhost:${port}`))
