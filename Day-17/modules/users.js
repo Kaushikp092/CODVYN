4a025923be7a6a485ff26016c5ec40b5c03ccf53
kaushikp092 <kaushikpatil092@gmail.com>
2026-02-03 19:30:33 +0530

    new schema

--- Day-17/modules/users.js
+++ Day-17/modules/users.js
@@ -1,36 +1,20 @@
 const mongoose = require('mongoose');
 
+// User schema aligned with auth routes (username + password)
 const userSchema = new mongoose.Schema({
-     name: {
+    username: {
         type: String,
         required: true,
     },
     email: {
         type: String,
-        required: true,
-    },
-    age: {
-        type: Number,
-        required: true
-    }
-});
-
-const UserAuthSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
     },
     password: {
         type: String,
-        required: true //hashed password
-    }
-})
+        required: true,
+        minLength: 6
+    },
+});
 
-const User = mongoose.model('User', userSchema);
-const AuthUser = mongoose.model('AuthUser', UserAuthSchema);
-
-module.exports = {User, AuthUser};
-
-
+module.exports = mongoose.model('User', userSchema);
 
