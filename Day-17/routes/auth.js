96a791b54909e6ae470a090f18a7280e035928ae
kaushikp092 <kaushikpatil092@gmail.com>
2026-02-03 19:30:20 +0530

    update in signup and login request

--- Day-17/routes/auth.js
+++ Day-17/routes/auth.js
@@ -1,54 +1,50 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { AuthUser } = require('../modules/users');
+const User = require('../modules/users');
 
 const router = express.Router();
 
-//Signup
+// Signup
 router.post('/signup', async (req, res) => {
     try {
-        const { username, password } = req.body;
-
-        //hash password
-        const hasedPassword = await bcrypt.hash(password, 10);
-
-        const user = new AuthUser({ username, password: hasedPassword })
-
-        await user.save();
-
-        res.status(201).json({ message: 'User created successfully' });
-
+        const { username, email, password } = req.body;
+        const hashed = await bcrypt.hash(password, 10);
+        await User.create({ username, email, password: hashed });
+        res.status(201).json({
+            message: 'Signup successful'
+        })
     } catch (err) {
-        res.status(500).json({ error: err.message })
+        res.status(400).json({
+            message: err.message
+        })
     }
-})
+});
 
-// Login
+//login
 router.post('/login', async (req, res) => {
     try {
         const { username, password } = req.body;
-
-        //finding user and storing in user var
-        const user = await AuthUser.findOne({ username });
-
+        const user = await User.findOne({ username })
         if (!user) return res.status(404).json({
-            error: 'User not found'
+            message: 'User not found'
         })
 
         const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) return res.status(404).json({
-            error: 'Invalid password'
+        if (!isMatch) return res.status(400).json({
+            message: 'invalid credentials'
         })
 
-        //create JWT 
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.status(200).json(token)
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '30min' });
+
+        res.status(200).json(token);
+
     } catch (err) {
-        res.status(500).json({ error: err.message })
+        res.status(400).json({
+            message: err.message
+        })
     }
-})
+});
 
 module.exports = router;
 
