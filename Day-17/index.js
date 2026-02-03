d4376cf363c88e24bb64179c9e8ca5eb9d0a5c00
kaushikp092 <kaushikpatil092@gmail.com>
2026-02-03 19:29:04 +0530

    updated index file

--- Day-17/index.js
+++ Day-17/index.js
@@ -1,20 +1,24 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth')
-const userRouter = require('./routes/users');
 const app = express();
+const userRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 require('dotenv').config();
+
 const port = process.env.PORT;
 
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.USER)
-    .then(() => console.log('MongoDB connected locally'))
-    .catch(err => console.log(err))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('MongoDB connected locally'))
+  .catch((err) => console.log('MongoDB connection error:', err.message));
 
 app.use('/api/users', userRouter);
 
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRouter);
 
-app.listen(port, () => console.log(`Example app listening http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening http://localhost:${port}`)
+);
