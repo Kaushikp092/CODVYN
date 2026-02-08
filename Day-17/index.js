5c2022c250eb5e5178687218d1283548419b4106
kaushikp092 <kaushikpatil092@gmail.com>
2026-02-08 14:32:25 +0530

    added auth to index.js file

--- Day-17/index.js
+++ Day-17/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-// const auth = require('./middleware/auth');
+const auth = require('./middleware/auth');
 const userRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 require('dotenv').config();
@@ -16,8 +16,7 @@ mongoose
   .then(() => console.log('MongoDB connected locally'))
   .catch((err) => console.log('MongoDB connection error:', err.message));
 
-// app.use('/api/users', auth, userRouter)
-app.use('/api/users', userRouter);
+app.use('/api/users', auth, userRouter)
 
 app.use('/api/auth', authRouter);
 
