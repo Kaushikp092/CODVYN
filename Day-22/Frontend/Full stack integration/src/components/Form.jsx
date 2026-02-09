20026a1173a810afe6e6c1a055ed87fedf256559
Kaushikp092 <kaushikpatil092@gmail.com>
2026-02-10 01:12:54 +0530

    form component

---
+++ Day-22/Frontend/Full stack integration/src/components/Form.jsx
@@ -0,0 +1,93 @@
+import "./Form.css";
+import { useState } from "react";
+
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState();
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState('');
+
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+
+    try{
+       const res = await fetch('http://localhost:5000/api/auth/login',{
+        method: "POST",
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({email, password})
+    });
+
+    if(!res.ok) throw new Error('Login Failed');
+
+    const data = await res.json();
+    setToken(data.token);
+    localStorage.setItem('jwt', data.token);
+    } catch (err) {
+      setError(err.message)
+    }
+  };
+   
+  const fetchUsers = async () => {
+    setError('');
+    const jwt = token || localStorage.getItem('jwt');
+    if(!jwt){
+      setError('No token found');
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:5000/api/users',{
+        headers: {Authorization: `Bearer ${jwt}`}
+      });
+
+      if(!res.ok) throw new Error('Failed to fetch users');
+
+      const data = await res.json();
+      setUsers(data);
+    } catch (err) {
+      setError(err.message);
+    }
+
+  };
+
+  return (
+    <>
+
+      <form onSubmit={handlesubmit}>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            />
+        </label>
+        <label htmlFor="password">Password:
+            <input type="password"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+            required />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+
+      {error && <p>{error}</p> }
+
+      <button onClick={fetchUsers}>Fetch Users</button>
+
+      <ul>
+        {
+          users.map((u)=>{
+            return <li key={u._id}>{u.email}</li>;
+          })
+        }
+      </ul>
+    </>
+  );
+};
+
+export default Form;
