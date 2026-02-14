5db109897740f0be556b4868baf84375c8740f29
Kaushikp092 <kaushikpatil092@gmail.com>
2026-02-14 21:24:10 +0530

    change in secret key variable name and status code

--- Day-22/Backend/routes/auth.js
+++ Day-22/Backend/routes/auth.js
@@ -17,7 +17,7 @@ router.post("/signup", async (req, res) => {
     res.status(201).json({message: 'User registered successfully'});
 
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
@@ -31,7 +31,7 @@ router.post('/login', async (req,res)=>{
         const isMatch = await bcrypt.compare(password, user.password);
         if(!isMatch) return res.status(400).json({message: 'invalid credentials'});
 
-        const token = jwt.sign({id: user._id}, process.env.SECRET, {expiresIn: '1d'});
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
 
         res.status(200).json({token})
         
