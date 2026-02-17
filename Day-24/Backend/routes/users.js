653102f8b049742a8760a3e30b3d3219e4b00e21
Kaushikp092 <kaushikpatil092@gmail.com>
2026-02-17 17:08:00 +0530

    clean code

--- Day-24/Backend/routes/users.js
+++ Day-24/Backend/routes/users.js
@@ -1,91 +1,27 @@
 const express = require("express");
-const User = require("../models/users");
-const authMiddleware = require("../middleware/auth");
-const bcrypt = require("bcrypt");
+const {
+  createUser,
+  getUsers,
+  getUsersByID,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userControllers");
+const authMiddleware = require('../middleware/auth')
 const router = express.Router();
 
 //GET all users /api/users (all routes protected)
-router.get("/", authMiddleware, async (req, res) => {
-  try {
-    const users = await User.find().select("-password");
-    if (!users)
-      return res.status(404).json({
-        message: "Users Not Found",
-      });
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.get("/", authMiddleware, getUsers);
 
 // GET user by ID
-router.get("/:id", authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id).select("-password");
-    if (!user)
-      return res.status(404).json({
-        message: "User not Found",
-      });
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
-  }
-});
+router.get("/:id", authMiddleware, getUsersByID);
 
 // POST Create a single user
-router.post("/", authMiddleware, async (req, res) => {
-  try {
-    const existingUser = await User.findOne({ email: req.body.email });
-    if (existingUser)
-      return res.status(400).json({ message: "Email already exists" });
-    const hashed = await bcrypt.hash(req.body.password, 10);
-    const user = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashed,
-    });
-
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.post("/", authMiddleware, createUser);
 
 // PUT update request
-router.put("/:id", authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    }).select("-password");
-    if (!user)
-      return res.status(404).json({
-        message: "User not found",
-      });
-    if (req.body.password) {
-      req.body.password = await bcrypt.hash(req.body.password, 10);
-    }
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
+router.put("/:id", authMiddleware, updateUser);
 
 // DELETE request
-router.delete("/:id", authMiddleware, async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    res.status(200).json({
-      message: "User deleted successfully",
-    });
-  } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
-  }
-});
+router.delete("/:id", authMiddleware, deleteUser);
 
 module.exports = router;
